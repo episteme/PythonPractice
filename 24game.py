2d11b006d1c703db6a8ec11110a840f0d6eca0c4
Marcus Peacock <marcus@marcuspeacock.com>
2015-09-09 12:16:00 +0100

    Added bracket support

--- 24game.py
+++ 24game.py
@@ -5,29 +5,33 @@ def main():
     print(nums)
     inp = raw_input()
     if validate(inp, nums):
-        ans = eval(inp)
+        try:
+            ans = eval(inp)
+        except SyntaxError:
+            print "Invalid expr, most likely bracket issue"
+            exit
         print(ans)
         if ans != 24:
             print "you lost"
         else:
             print "you won"
     
-
 def four_numbers():
     return (str(randint(0,9)) + str(randint(0,9))
             + str(randint(0,9)) + str(randint(0,9))) 
 
 def validate(expr, nums):
     valid = ['+','-','/','*']
+    expr_clean = expr.replace("(", "").replace(")", "")
     nums_in_expr = []
-    if len(expr) != 7:
+    if len(expr_clean) != 7:
         print "invalid expr"
         return False
-    for c in range(0, len(expr)):
+    for c in range(0, len(expr_clean)):
         if c % 2 == 0:
-            nums_in_expr.append(expr[c])
+            nums_in_expr.append(expr_clean[c])
         else:
-            if expr[c] not in valid:
+            if expr_clean[c] not in valid:
                 print "invalid operator"
                 return False
     if sorted(nums) != sorted(nums_in_expr):
